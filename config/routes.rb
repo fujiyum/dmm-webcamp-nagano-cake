dd6873d7aff104609fc2f547f82a1a0410037827
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-10-31 06:13:27 +0000

    [Update]item/controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, except: [:destroy]
   end
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_123132) do
+ActiveRecord::Schema.define(version: 2021_10_31_055746) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2021_10_30_123132) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
