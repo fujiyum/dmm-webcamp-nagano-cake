3e5a35a9510b6a3ec651305ba339031768086238
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-11-01 12:31:46 +0000

    [Update]商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item_path(item.id)
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -27,7 +27,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
+    params.require(:item).permit(:name, :image, :introduction, :genre_id, :price, :is_active)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_055746) do
+ActiveRecord::Schema.define(version: 2021_11_01_120715) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_055746) do
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
   end
 
 end
