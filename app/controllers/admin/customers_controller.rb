b523042c2cf00592241221817b50cf431aedb004
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-11-03 02:58:16 +0000

    [Update]会員詳細画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,12 +5,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.params([:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:customer_id, :last_name, :first_name, :last_name_kana, :first_name_kana, :postale_code, :address, :telephone_number, :email, :is_active)
   end
 
 end
