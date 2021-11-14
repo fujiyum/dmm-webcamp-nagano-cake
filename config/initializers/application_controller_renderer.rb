6abf1681080ef89a7c3152759706804054913d57
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-11-14 13:51:38 +0000

    [Update]商品画像投稿

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ff47ac9571c3968b8c1c7947832dabc91b5800f9ece62d9f74f4baeb8e4306d7dbddb1eecd775362b583bbd71dd7914ab2f0f549c1bc666c4de7c059e7c7491f'
