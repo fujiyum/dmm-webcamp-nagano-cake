998a6785f75855f7b59613826a9a115c14bea2d0
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-11-28 03:51:52 +0000

    [Update]admin/top画面

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,19 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_customer
 
  def top
-   @orders = Order.all
+   if params[:customer_id] == @customer.ids
+    @orders = @customer.orders.all
+   else
+    @orders = Order.all
+   end
+ end
+
+ private
+
+ def set_customer
+   @customer = Customer.all
  end
 
 end
