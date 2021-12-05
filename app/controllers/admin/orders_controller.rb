aedbf51ae8ec62daefd69cd8dc9cef7f6484c6ac
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-12-05 02:08:45 +0000

    [Update]ステータスの更新機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,8 +8,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to admin_order_path(@order.id)
+    @order_details = @order.order_details
+    if @order.update(order_params)
+       @order.order_details.update_all(making_status: "製作待ち") if @order.status == "入金確認"
+       redirect_to admin_order_path(@order.id)
+    else
+      redirect_to admin_order_path(@order.id)
+    end
   end
 
   private
