1c364209a3901cae6375f814c314887773d9c4d5
EC2 Default User <91727339+fujiyum@users.noreply.github.com>
2021-12-05 07:57:51 +0000

    [Update]ステータス機能

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -6,6 +6,14 @@ class Admin::OrderDetailsController < ApplicationController
    @order = @order_detail.order
    @order_details = @order.order_details
    @order_detail.update(order_detail_params)
+   if @order_detail.making_status == "make"
+      @order.update(status: :production)
+   end
+
+   if @order.order_details.count == @order_details.where(making_status: "completed").count
+      @order.status = "ready"
+      @order.save
+   end
    redirect_to admin_order_path(@order_detail.order.id)
   end
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,12 +9,13 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order_details = @order.order_details
-    if @order.update(order_params)
-       @order.order_details.update_all(making_status: "製作待ち") if @order.status == "入金確認"
-       redirect_to admin_order_path(@order.id)
-    else
-      redirect_to admin_order_path(@order.id)
+    @order.update(order_params)
+    if @order.status == "confirmation"
+       @order_details.each do |order_detail|
+         order_detail.update(making_status: :wait)
+       end
     end
+      redirect_to admin_order_path(@order.id)
   end
 
   private
